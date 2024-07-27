3ef20481c492c4a117b98960c75a1573eb209948
lex_xenda <57144323+atogebaniaalexander@users.noreply.github.com>
2024-07-27 23:41:50 +0000

    changes

--- dist/src/Helpers/prismaController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.executePrismaMethod = executePrismaMethod;
-async function executePrismaMethod(prisma, entityName, methodName, options = {}) {
-    return prisma[entityName][methodName](options);
-}
-//# sourceMappingURL=prismaController.js.map
