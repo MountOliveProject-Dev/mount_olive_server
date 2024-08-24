88b135d1aaf0e81a6bb3c7f2670cbdca0fa167e5
Atogebania Alexander <aatogebania@gmail.com>
2024-08-24 14:14:38 +0000

    notifications worked on

--- dist/src/Handlers/notificationHandlers.js
+++ dist/src/Handlers/notificationHandlers.js
@@ -42,10 +42,10 @@ const listNotificationsHandler = async (request, h) => {
             let notificationMedia = {};
             for (let j = 0; j < getMedia.length; j++) {
                 if (getMedia[j].notificationId === notificationId && (getMedia[j].mediaId !== null || getMedia[j].mediaId !== undefined)) {
-                    notificationMedia.push(getMedia[j].media);
+                    notificationMedia = getMedia[j].media;
                 }
                 else if (getMedia[j].notificationId === notificationId && (getMedia[j].eventId !== null || getMedia[j].eventId !== undefined)) {
-                    notificationMedia.push(getMedia[j].event);
+                    notificationMedia = getMedia[j].event;
                 }
             }
             if (getMedia[i].videoStatus === true) {
